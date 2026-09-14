726ed408817aa6599e9fb2d13257c01fad50c261
Arthur-Leywin13 <louistephens08@gmail.com>
2026-09-14 17:54:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express')
 const sessionManager = require('./lib/sessionManager')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Jack WhatsApp Bot is online')
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur HTTP lancé sur le port ${PORT}`)
+})
 
 sessionManager.bootAll().catch((err) => {
   console.error('Erreur au démarrage des sessions:', err)
